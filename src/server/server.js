182257d24a1b7e3b494c5040bbdd9ca87611ac46
zainahmad0580 <zainahmad0580@gmail.com>
2023-05-17 14:57:36 +0500

    server commit

---
+++ src/server/server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+
+app.get('/api', (req, res) => {
+    res.json({'users': ['user1', 'user2', 'user3', 'user4', 'user5']});
+})
+
+app.listen(5000, () => {
+    console.log('Server started on port 5000')
+})
+
